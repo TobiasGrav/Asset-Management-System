ef53ad7c6fa988d2eb9933ebc200c0e432e386b5
TobiasGrav <tobiagra@stud.ntnu.no>
2024-03-16 20:05:38 +0100

    Initial spring boot

---
+++ src/main/java/ntnu/group03/Bachelor_IDATA2900_AMS/AssetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ntnu.group03.Bachelor_IDATA2900_AMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntnu/group03/Bachelor_IDATA2900_AMS/AssetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.group03.Bachelor_IDATA2900_AMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
