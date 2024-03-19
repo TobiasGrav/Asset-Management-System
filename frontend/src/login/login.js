595a7bc19051898acd73086018636ac5f1a5583c
TobiasGrav <tobiagra@stud.ntnu.no>
2024-03-19 20:59:44 +0100

    Added a login page on the frontend, this page is not functional yet.

--- frontend/src/login/login.js
+++ frontend/src/login/login.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './login.css'
 
 const Login = (props) => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  const login = () => {
+    console.log(email);
+    console.log(password);
+  }
+
   return (
     <div className="login-container">
       <div className="mainLogin">
@@ -14,18 +23,18 @@ const Login = (props) => {
           />
           <span className="logoText">Asset Management Database</span>
         </div>
-        <form className="loginForm">
+        <form onSubmit={login} className="loginForm">
           <span>Log in</span>
-          <input type="text" placeholder="Email" className="input" />
-          <input type="text" placeholder="Password" className="input" />
+          <input type="text" id='emailInput' placeholder="Email" className="input" value={ email } onChange={ (e) => setEmail(e.target.value) } required/>
+          <input type="text" id='passwordInput' placeholder="Password" className="input" value={ password } onChange={(e) => setPassword(e.target.value)} required/>
           <a
-            href="https://example.com"
+            href="localhost:8080/recover"
             target="_blank"
             rel="noreferrer noopener"
           >
             Forgot your password?
           </a>
-          <button type="button" className="button">
+          <button onClick={login} type='button' className="button">
             <span>
               <span>Login</span>
               <br></br>
