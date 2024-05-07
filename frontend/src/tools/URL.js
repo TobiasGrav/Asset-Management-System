86b7adf114704d99702adacd46dbac974c14cac8
TobiasGrav <tobiagra@stud.ntnu.no>
2024-05-07 12:57:00 +0200

    finished the cd on main

--- frontend/src/tools/URL.js
+++ frontend/src/tools/URL.js
@@ -1,6 +1,6 @@
 const URL = {
-    BACKEND: "https://asset-management-system-6.norwayeast.cloudapp.azure.com",
-    FRONTEND: "https://asset-management-system-6.norwayeast.cloudapp.azure.com"
+    BACKEND: "https://asset-management-system.norwayeast.cloudapp.azure.com",
+    FRONTEND: "https://asset-management-system.norwayeast.cloudapp.azure.com"
 };
 
 export default URL;
