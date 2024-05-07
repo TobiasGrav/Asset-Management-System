1a270812e1ee55c06f5d0fe00adada7e2699ab98
TobiasGrav <tobiagra@stud.ntnu.no>
2024-05-07 09:25:13 +0200

    changed domain

--- vminit.sh
+++ vminit.sh
@@ -27,12 +27,12 @@ sudo docker login -u amsprojectacr -p "${ACR_PASSWORD}" amsprojectacr.azurecr.io
 
 sudo docker network create ams-network
 
-sudo certbot certonly --standalone -d asset-management-system-5.norwayeast.cloudapp.azure.com --non-interactive --agree-tos --email tobiagra@stud.ntnu.no
+sudo certbot certonly --standalone -d asset-management-system-6.norwayeast.cloudapp.azure.com --non-interactive --agree-tos --email tobiagra@stud.ntnu.no
 
 #sudo docker pull certbot/certbot:latest
 #sudo docker run -it -p 80:80 -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -v "/var/log/letsencrypt:/var/log/letsencrypt" -v "/etc/nginx:/etc/nginx" -v "/var/www/certbot:/var/www/certbot" certbot/certbot certonly --standalone -d asset-management-system-5.norwayeast.cloudapp.azure.com --non-interactive --agree-tos --email tobiagra@stud.ntnu.no
 sudo docker pull amsprojectacr.azurecr.io/ams-backend:latest
-sudo docker run -d --name ams-backend -p 8080:8080 -e DATABASE_URL=ams-db -e DATABASE_USERNAME=root -e DATABASE_PASSWORD="${MYSQL_PASSWORD}" --network ams-network amsprojectacr.azurecr.io/ams-backend:latest
+sudo docker run -d --name ams-backend -p 8080:8080 -e DATABASE_URL="51.13.61.6" -e DATABASE_USERNAME=root -e DATABASE_PASSWORD="${MYSQL_PASSWORD}" --network ams-network amsprojectacr.azurecr.io/ams-backend:latest
 
 sudo docker pull amsprojectacr.azurecr.io/ams-nginx:latest
 sudo docker run -d --name ams-nginx -p 80:80 -p 443:443 -v "/etc/letsencrypt:/etc/letsencrypt" --network ams-network amsprojectacr.azurecr.io/ams-nginx:latest
