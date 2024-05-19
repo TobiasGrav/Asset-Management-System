45a6123d40423ea709e17bd1720cb2d067e5838a
TobiasGrav <tobiagra@stud.ntnu.no>
2024-05-20 01:24:15 +0200

    edited init bash script

--- vminit.sh
+++ vminit.sh
@@ -22,7 +22,7 @@ sudo apt-get install -y certbot
 
 ACR_PASSWORD=$(sudo az keyvault secret show --name amsprojectacrpassword --vault-name ams-secret-key-vault --query value -o tsv)
 MYSQL_PASSWORD=$(sudo az keyvault secret show --name mysqlpassword --vault-name ams-secret-key-vault --query value -o tsv)
-
+JWTSECRET=$(sudo az keyvault secret show --name JWTSECRET --vault-name ams-secret-key-vault --query value -o tsv)
 
 
 sudo docker login -u amsprojectacr -p "${ACR_PASSWORD}" amsprojectacr.azurecr.io
@@ -34,7 +34,7 @@ sudo certbot certonly --standalone -d mngsys.norwayeast.cloudapp.azure.com --non
 #sudo docker pull certbot/certbot:latest
 #sudo docker run -it -p 80:80 -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -v "/var/log/letsencrypt:/var/log/letsencrypt" -v "/etc/nginx:/etc/nginx" -v "/var/www/certbot:/var/www/certbot" certbot/certbot certonly --standalone -d asset-management-system-5.norwayeast.cloudapp.azure.com --non-interactive --agree-tos --email tobiagra@stud.ntnu.no
 sudo docker pull amsprojectacr.azurecr.io/ams-backend:latest
-sudo docker run -d --name ams-backend -p 8080:8080 -e DATABASE_URL="51.13.61.6" -e DATABASE_USERNAME=root -e DATABASE_PASSWORD="${MYSQL_PASSWORD}" --network ams-network amsprojectacr.azurecr.io/ams-backend:latest
+sudo docker run -d --name ams-backend -p 8080:8080 -e DATABASE_URL="51.13.61.6" -e DATABASE_USERNAME=root -e DATABASE_PASSWORD="${MYSQL_PASSWORD}" -e DATABASE_PASSWORD="${JWTSECRET}" --network ams-network amsprojectacr.azurecr.io/ams-backend:latest
 
 sudo docker pull amsprojectacr.azurecr.io/ams-nginx:latest
 sudo docker run -d --name ams-nginx -p 80:80 -p 443:443 -v "/etc/letsencrypt:/etc/letsencrypt" --network ams-network amsprojectacr.azurecr.io/ams-nginx:latest
